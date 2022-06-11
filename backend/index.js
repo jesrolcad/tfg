41ad763ee68d53858daf359a48ffa214eb018094
luctorgom <luctorgom@alum.us.es>
2022-06-11 20:33:37 +0200

    Proyecto práctica MEVN
    Pruebas para comprender los conceptos clave del stack.
    
    Torres Gómez, Lucía <@luctorgom>

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,37 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-
-//const router = require('./api/router');
-
+const morgan = require('morgan');
+/*const dotenv = require("dotenv");
+dotenv.config();*/
 require("dotenv").config();
 
-app.use(express.json());
-app.use(cors());
 
-const connection_string = process.env.CONNECTION_STRING
-
+//Server is listening
 app.listen(5000, () => {
     console.log('listening on port 5000')
 })
 
-//app.use('/api/v1', router);
+//Conexion MongoDB
+mongoose.Promise = global.Promise;
 
-// app.listen(process.env.EXPRESS_PORT, function() {
-//     console.log("App running at http://localhost:" + process.env.EXPRESS_PORT);
-//     console.log("________________________________________________________________");
-// });
-
+const connection_string = process.env.CONNECTION_STRING;
 mongoose.connect(connection_string, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 .then(() => {console.log("MongoDB connection established")})
 .catch((error) => {console.error("MongoDB connection failed:",error.message)})
+
+//Static files
+app.use(express.static('/Users/usuario/TFG/tfg/public'));
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+
+//Routes
+app.use('/programas', require('./routes/pelis'));
+
