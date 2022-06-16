dd2ece500902d958da58f46b23302a09d6c0175f
luctorgom <luctorgom@alum.us.es>
2022-06-16 13:44:35 +0200

    PAGINACIÓN CREADA
    Se ha creado una paginación para la vista del listado de programas. Aunque también pueder servir para futuras vistas.
    
    Torres Gómez, Lucía <@luctorgom>

--- backend/api/services/programas.js
+++ backend/api/services/programas.js
@@ -4,6 +4,6 @@ const Programa=require('../../models/Programa');
 module.exports.getAllProgramas = async (req,res) => {
     const programas= await Programa.find()
         .select({ "titulo": 1, "tipo":1, "fecha":1, "imagen":1, "_id": 0})
-        .sort({fecha:'desc'}).limit(21);
+        .sort({fecha:'desc'});
     res.json(programas);
 }
--- src/views/Programas.vue
+++ src/views/Programas.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="card-group" >
         <div class="col-3" style="margin: 20px;font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,
-        Helvetica Neue,sans-serif;border-radius: 20px;box-shadow: 0px 0px 5px var(--bs-gray-400);border-style: none;"  v-for="programa of programas">
+        Helvetica Neue,sans-serif;border-radius: 20px;box-shadow: 0px 0px 5px var(--bs-gray-400);border-style: none;"  v-for="programa of displayedProgramas">
             <img class="card-img-top w-100 d-block" style="border-radius: 20px;border-bottom-right-radius: 0px;border-bottom-left-radius: 0px;" :src="programa.imagen || 'placeholder.png'"> 
             <div class="card-body" style="margin: 10px">
                 <h4 class="card-title">{{programa.titulo}}</h4>
@@ -13,6 +13,27 @@
             </div>
         </div>
     </div>
+    <div class="btn-group col-md-2 offset-md-5">
+        <button
+          type="button"
+          class="btn btn-sm btn-outline-secondary"
+          v-if="page != 1"
+          @click="page--"
+        >&lt;&lt;</button>
+        <button
+          type="button"
+          class="btn btn-sm btn-outline-secondary"
+          v-for="pageNumber in pages.slice(page-1, page+5)"
+          @click="page = pageNumber"
+          :key="pageNumber"
+        >{{pageNumber}}</button>
+        <button
+          type="button"
+          @click="page++"
+          v-if="page < pages.length"
+          class="btn btn-sm btn-outline-secondary"
+        >>></button>
+      </div>
 </template>
 <script>
     import moment from 'moment'
@@ -34,7 +55,11 @@
         data(){
             return{
                 programa: new Programa(),
-                programas:[]
+                programas:[],
+                baseURL: "http://localhost:5000",
+                page: 1,
+                perPage: 21,
+                pages: []
             }
         },
         created(){
@@ -42,13 +67,36 @@
         },
         methods:{
             getProgramas(){
-                fetch('http://localhost:5000/programas/all')
+                fetch(this.baseURL+'/programas/all')
                     .then(res=> res.json())
                     .then(data => {
                         this.programas=data;
                     });
             },
-            moment
+            moment,
+            setProgramas() {
+                let numberOfPages = Math.ceil(this.programas.length / this.perPage);
+                for (let i = 1; i <= numberOfPages; i++) {
+                    this.pages.push(i);
+            }
+            },
+            paginate(programas) {
+                let page = this.page;
+                let perPage = this.perPage;
+                let from = (page * perPage) - perPage;
+                let to = (page * perPage);
+                return programas.slice(from, to);
+            }
+        },
+        watch: {
+            programas(){
+                this.setProgramas();
+            }
+        },
+        computed: {
+            displayedProgramas: function () {
+            return this.paginate(this.programas);
+            }
         }
     }
 </script>
