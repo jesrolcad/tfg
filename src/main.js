4091e6bb4de4b8d3033a12e65fcbb62dbaf6103a
luctorgom <luctorgom@alum.us.es>
2022-06-16 11:49:34 +0200

    INSTALACIÓN BOOTSTRAP 5
    Se instala bootstrap 5 y se desestima el uso de Bootstrap-Vue por incompatibiliad con Vue 3.
    
    Torres Gómez, Lucía <@luctorgom>
    
    Co-authored-by: Jesús Roldán Cadena <@jesrolcad>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue';
 import router from "./router/index";
 
 createApp(App).use(router).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
 
 
