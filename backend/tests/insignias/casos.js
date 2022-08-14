6fc81a3597ecf6036dbf5d47fa2878f4ffe5a1e7
luctorgom <luctorgom@alum.us.es>
2022-08-14 20:51:46 +0200

    TEST CASE INSIGNIAS
    Testeados todos los métodos de insignias.
    
    Torres Gómez, Lucía <@luctorgom>

--- backend/tests/insignias/casos.js
+++ backend/tests/insignias/casos.js
@@ -12,46 +12,195 @@ const casosPositivosGetInsigniasUsuario = [
         nombreUsuario: 'anotherUserTester',
         insigniasEsperadas: 1
     },
+    {
+        key: 'OBTENER INSIGNIAS USUARIO usuarioLista',
+        nombreUsuario: 'usuarioLista',
+        insigniasEsperadas: 15
+    }
 ]
 // CASOS RECIBIR UNA INSIGNIA LISTAS PROPIAS
 const casosPositivosGetInsigniasListasPropias = [
 
     {
         key: 'OBTENER INSIGNIA LIST BEGINNER',
-        nombreUsuario: 'usuarioInsignia',
-        listaPropia: 1
+        nombreUsuario: 'userTester',
+        listaPropia: [{listasPropias: 1}]
     },
 
     {
         key: 'OBTENER INSIGNIA LIST EXPERT',
         nombreUsuario: 'userTester',
-        listaPropia: 3
+        listaPropia: [{listasPropias: 3}]
     },
 
     {
         key: 'OBTENER INSIGNIA LIST FREAK',
-        nombreUsuario: 'usuarioLista',
-        listaPropia: 5
+        nombreUsuario: 'userTester',
+        listaPropia: [{listasPropias: 5}]
     },
 
     {
         key: 'TODAS LAS INSIGNIAS',
-        nombreUsuario: 'usuarioAllInsignias',
-        listaPropia: 9
+        nombreUsuario: 'usuarioLista',
+        listaPropia: [{listasPropias: 9}]
 
     },
 
     {
         key: 'YA TIENE LIST BEGINNER',
         nombreUsuario: 'anotherUserTester',
-        listaPropia: 0
+        listaPropia: [{listasPropias: 0}]
     },
-    
+
     {
         key: 'SIN LISTA PROPIA',
-        nombreUsuario: 'usuarioSinListaP',
-        listaPropia: -1
+        nombreUsuario: 'usuarioCuatro',
+        listaPropia:[]
     },
 ]
 
-module.exports = {casosPositivosGetInsigniasUsuario, casosPositivosGetInsigniasListasPropias}
+// CASOS RECIBIR UNA INSIGNIA PROGRAMAS LISTAS PROPIAS
+const casosPositivosGetInsigniasProgramasLP = [
+
+    {
+        key: 'OBTENER INSIGNIA BRONCE LIST',
+        nombreUsuario: 'userTester',
+        result: [{programas: [1,2,3,4,5]},{programas: [1,2,3,4,5,6,7,8]}],
+        insignia: "Bronce"
+    },
+
+    {
+        key: 'OBTENER INSIGNIA SILVER LIST AND GOLD LIST Y YA TIENE BRONCE',
+        nombreUsuario: 'userTester',
+        result: [{programas: [1,2,3,4,5,6,7,8,9,10]},{programas: [1,2,3,4,5]},
+        {programas: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]},{programas: [1,2,3]}],
+        insignia: "Plata, Oro"
+    },
+
+    {
+        key: 'TODAS LAS INSIGNIAS',
+        nombreUsuario: 'usuarioLista',
+        result: [{programas: [1,2,3,4,5,6,7,8,9,10]},{programas: [1,2,3,4,5]},
+        {programas: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]}],
+        insignia: "Todas"
+
+    },
+
+    {
+        key: 'SIN LISTA INSIGNIA',
+        nombreUsuario: 'usuarioCuatro',
+        result:[]
+    },
+]
+
+// CASOS RECIBIR UNA INSIGNIA ACTORES
+const casosPositivosGetInsigniasActores = [
+
+    {
+        key: 'OBTENER INSIGNIA Actor/Actress Fan',
+        nombreUsuario: 'userTester',
+        result: [{ "_id": "Stan Lee", "count": 1},{"_id": "Chris Evans", "count": 3}],
+        insignia: "Fan"
+    },
+
+    {
+        key: 'OBTENER INSIGNIA Actor/Actress Lover AND Actor/Actress Addict Y YA Actor/Actress Fan',
+        nombreUsuario: 'userTester',
+        result: [{"_id": "Chris Evans", "count": 3},{"_id": "Scarlett Johansson", "count": 5},{"_id": "Robert Downey Jr.", "count": 10}],
+        insignia: "Lover, Addict"
+    },
+
+    {
+        key: 'TODAS LAS INSIGNIAS',
+        nombreUsuario: 'usuarioLista',
+        result: [{"_id": "Chris Evans", "count": 3},{"_id": "Scarlett Johansson", "count": 5},{"_id": "Robert Downey Jr.", "count": 10}],
+        insignia: "Todas"
+
+    },
+
+    {
+        key: 'SIN LISTA INSIGNIA',
+        nombreUsuario: 'usuarioCuatro',
+        result: []
+    },
+]
+
+const casosPositivosGetInsigniasProgramasV = [
+
+    {
+        key: 'OBTENER INSIGNIA Bronce Watcher',
+        nombreUsuario: 'userTester',
+        result: [{programa: 10}],
+        insignia: "Bronce"
+    },
+
+    {
+        key: 'OBTENER INSIGNIA Silver Watcher',
+        nombreUsuario: 'userTester',
+        result: [{programa: 25}],
+        insignia: "Silver"
+    },
+
+    {
+        key: 'SIN INSIGNIA NUEVA',
+        nombreUsuario: 'userTester',
+        result: [{programa: 42}],
+        insignia: "Otro"
+    },
+
+    {
+        key: 'OBTENER INSIGNIA Gold Watcher',
+        nombreUsuario: 'userTester',
+        result: [{programa: 50}],
+        insignia: "Gold"
+    },
+
+    {
+        key: 'TODAS LAS INSIGNIAS',
+        nombreUsuario: 'usuarioLista',
+        result: [{programa: 100}],
+        insignia: "Todas"
+
+    },
+
+    {
+        key: 'SIN INSIGNIA PROGRAMA V',
+        nombreUsuario: 'usuarioCuatro',
+        result: []
+    },
+]
+
+const casosPositivosGetInsigniasGeneros = [
+
+    {
+        key: 'OBTENER INSIGNIA Amateur Genre Watcher',
+        nombreUsuario: 'userTester',
+        result: [{"_id": "Familia","count": 5}],
+        insignia: "Amateur"
+    },
+
+    {
+        key: 'OBTENER INSIGNIA Intermediate Genre Watcher AND Professional Genre Watcher Y YA Amateur Genre Watcher',
+        nombreUsuario: 'userTester',
+        result: [{"_id": "Ciencia ficción", "count": 10},{"_id": "Comedia", "count": 20},{"_id": "Familia","count": 5}],
+        insignia: "Intermediate, Professional"
+    },
+
+    {
+        key: 'TODAS LAS INSIGNIAS',
+        nombreUsuario: 'usuarioLista',
+        result: [{"_id": "Ciencia ficción", "count": 15},{"_id": "Comedia", "count": 20},{"_id": "Familia","count": 5}],
+        insignia: "Todas"
+
+    },
+
+    {
+        key: 'SIN INSIGNIA',
+        nombreUsuario: 'usuarioCuatro',
+        result: []
+    },
+]
+
+module.exports = {casosPositivosGetInsigniasUsuario, casosPositivosGetInsigniasListasPropias,
+    casosPositivosGetInsigniasProgramasLP,casosPositivosGetInsigniasActores,
+    casosPositivosGetInsigniasProgramasV, casosPositivosGetInsigniasGeneros}
--- backend/tests/insignias/insignias.test.js
+++ backend/tests/insignias/insignias.test.js
@@ -3,10 +3,9 @@ const { app, server } = require('../../index');
 const request = supertest(app);
 const { connectDB, disconnectDB, setupData } = require('../mock_database_configuration');
 const Lista = require('../../models/Lista');
-const Usuario = require('../../models/Usuario');
-const Programa = require('../../models/Programa');
-const insigniasService = require('../../api/services/insignias');
-const { casosPositivosGetInsigniasUsuario, casosPositivosGetInsigniasListasPropias } = require('./casos');
+const { casosPositivosGetInsigniasUsuario, casosPositivosGetInsigniasListasPropias,
+    casosPositivosGetInsigniasProgramasLP, casosPositivosGetInsigniasActores,
+    casosPositivosGetInsigniasProgramasV, casosPositivosGetInsigniasGeneros} = require('./casos');
 
 describe('TESTS INSIGNIAS', () => {
 
@@ -33,56 +32,203 @@ describe('TESTS INSIGNIAS', () => {
                 const token = response_login.body.token;
                 const response = await request.get('/insignias/usuario').set('Authorization', token);
 
-                if (caso.insigniasEsperadas == 0) {
-                    expect(response.status).toBe(200);
-                    expect(response.body.length).toBe(0);
-                }
-                else if (caso.insigniasEsperadas == 1) {
-                    expect(response.status).toBe(200);
-                    expect(response.body.length).toBe(1);
-                }
+                expect(response.status).toBe(200);
+                expect(response.body.length).toBe(caso.insigniasEsperadas);
         })
     })
 
     describe('TESTS AÑADIR INSIGNIA LISTAS PROPIAS', () => {
+        for (const caso of casosPositivosGetInsigniasListasPropias)
 
-        describe('CASOS POSITIVOS', () => {
-
-            for (const caso of casosPositivosGetInsigniasListasPropias)
-
-                it(caso.key, async () => {
-                    const response_login = await request.post('/usuarios/login').send({
-                        nombreUsuario: caso.nombreUsuario,
-                        password: '12345678'
-                    })
-                    const token = response_login.body.token;
-                    const response = await request.put('/insignias/listas').set('Authorization', token);
-
-                    if (caso.listaPropia == 1) {
-                        expect(response.status).toBe(200);
-                        expect(response.body.insignia).toBe('List Beginner');
-                    }
-                    else if (caso.listaPropia == 3) {
-                        expect(response.status).toBe(200);
-                        expect(response.body.insignia).toBe('List Expert');
-                    }
-                    else if (caso.listaPropia == 5) {
-                        expect(response.status).toBe(200);
-                        expect(response.body.insignia).toBe('List Freak');
-                    }
-                    else if (caso.listaPropia == 0) {
-                        expect(response.status).toBe(200);
-                        expect(response.body.mensaje).toBe('No hay insignias. Listas propias: 1');
-                    }
-                    else if (caso.listaPropia == 9) {
-                        expect(response.status).toBe(200);
-                        expect(response.body.mensaje).toBe('Este usuario ya posee todas las insignias de listas propias.');
-                    }
-                    else if (caso.listaPropia == -1) {
-                        expect(response.status).toBe(200);
-                        expect(response.body.mensaje).toBe('No hay insignias. Listas propias: 0');
-                    }
+            it(caso.key, async () => {
+                const response_login = await request.post('/usuarios/login').send({
+                    nombreUsuario: caso.nombreUsuario,
+                    password: '12345678'
                 })
-        })
+                const token = response_login.body.token;
+                if(caso.listaPropia){
+                    const mock= jest.spyOn(Lista, 'aggregate').mockImplementation(()=>{
+                        return caso.listaPropia
+                    });
+                }else{
+                    const mock= jest.spyOn(Lista, 'aggregate').mockImplementation(()=>{
+                        return []
+                    });
+                }
+                const response = await request.put('/insignias/listas').set('Authorization', token);
+
+                if(!caso.listaPropia){
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('No hay insignias. Listas propias: 0');
+                }
+                else if (caso.listaPropia.listasPropias == 1) {
+                    expect(response.status).toBe(200);
+                    expect(response.body.insignia).toBe('List Beginner');
+                }
+                else if (caso.listaPropia.listasPropias == 3) {
+                    expect(response.status).toBe(200);
+                    expect(response.body.insignia).toBe('List Expert');
+                }
+                else if (caso.listaPropia.listasPropias == 5) {
+                    expect(response.status).toBe(200);
+                    expect(response.body.insignia).toBe('List Freak');
+                }
+                else if (caso.listaPropia.listasPropias == 0) {
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('No hay insignias. Listas propias: '+ caso.listaPropia.listasPropias);
+                }
+                else if (caso.listaPropia.listasPropias == 9) {
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('Este usuario ya posee todas las insignias de listas propias.');
+                }
+            })
+    })
+
+    describe('TESTS AÑADIR INSIGNIA PROGRAMAS LISTAS PROPIAS', () => {
+        for (const caso of casosPositivosGetInsigniasProgramasLP)
+
+            it(caso.key, async () => {
+                const response_login = await request.post('/usuarios/login').send({
+                    nombreUsuario: caso.nombreUsuario,
+                    password: '12345678'
+                })
+                const token = response_login.body.token;
+                const mock= jest.spyOn(Lista, 'aggregate').mockImplementation(()=>{
+                    return caso.result
+                });
+
+                const response = await request.put('/insignias/listas-programas').set('Authorization', token);
+
+                if (caso.insignia == "Bronce") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.Bronce).toBe(true);
+                }
+                else if (caso.insignia == "Plata, Oro") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.Bronce).toBe(false);
+                    expect(response.body.Silver).toBe(true);
+                    expect(response.body.Gold).toBe(true);
+                }
+                else if (caso.insignia == "Todas") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('Este usuario ya posee todas las insignias de programas en listas propias.');
+                }
+                else{
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('No hay insignias.');
+                }
+            })
+    })
+
+    describe('TESTS AÑADIR INSIGNIA ACTORES', () => {
+        for (const caso of casosPositivosGetInsigniasActores)
+
+            it(caso.key, async () => {
+                const response_login = await request.post('/usuarios/login').send({
+                    nombreUsuario: caso.nombreUsuario,
+                    password: '12345678'
+                })
+                const token = response_login.body.token;
+                const mock= jest.spyOn(Lista, 'aggregate').mockImplementation(()=>{
+                    return caso.result
+                });
+
+                const response = await request.put('/insignias/actores').set('Authorization', token);
+
+                if (caso.insignia == "Fan") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.Fan).toBe(true);
+                }
+                else if (caso.insignia == "Lover, Addict") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.Fan).toBe(false);
+                    expect(response.body.Lover).toBe(true);
+                    expect(response.body.Addict).toBe(true);
+                }
+                else if (caso.insignia == "Todas") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('Este usuario ya posee todas las insignias de actores.');
+                }
+                else{
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('No hay insignias.');
+                }
+            })
+    })
+
+    describe('TESTS AÑADIR INSIGNIA PROGRAMAS VISTOS', () => {
+        for (const caso of casosPositivosGetInsigniasProgramasV)
+
+            it(caso.key, async () => {
+                const response_login = await request.post('/usuarios/login').send({
+                    nombreUsuario: caso.nombreUsuario,
+                    password: '12345678'
+                })
+                const token = response_login.body.token;
+                const mock= jest.spyOn(Lista, 'aggregate').mockImplementation(()=>{
+                    return caso.result
+                });
+                const response = await request.put('/insignias/programas').set('Authorization', token);
+
+                if (caso.insignia == "Bronce") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.insignia).toBe('Bronce Watcher');
+                }
+                else if (caso.insignia == "Silver") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.insignia).toBe('Silver Watcher');
+                }
+                else if (caso.insignia == "Otro") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('No hay insignias. Programas vistos: '+ caso.result[0].programa);
+                }
+                else if (caso.insignia == "Gold") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.insignia).toBe('Gold Watcher');
+                }
+                else if (caso.insignia == "Todas") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('Este usuario ya posee todas las insignias de programas vistos.');
+                }else{
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('No hay insignias. Programas vistos: 0');
+                }
+            })
+    })
+
+    describe('TESTS AÑADIR INSIGNIA GENEROS', () => {
+        for (const caso of casosPositivosGetInsigniasGeneros)
+
+            it(caso.key, async () => {
+                const response_login = await request.post('/usuarios/login').send({
+                    nombreUsuario: caso.nombreUsuario,
+                    password: '12345678'
+                })
+                const token = response_login.body.token;
+                const mock= jest.spyOn(Lista, 'aggregate').mockImplementation(()=>{
+                    return caso.result
+                });
+
+                const response = await request.put('/insignias/generos').set('Authorization', token);
+
+                if (caso.insignia == "Amateur") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.Amateur).toBe(true);
+                }
+                else if (caso.insignia == "Intermediate, Professional") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.Amateur).toBe(false);
+                    expect(response.body.Intermediate).toBe(true);
+                    expect(response.body.Professional).toBe(true);
+                }
+                else if (caso.insignia == "Todas") {
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('Este usuario ya posee todas las insignias de generos.');
+                }
+                else{
+                    expect(response.status).toBe(200);
+                    expect(response.body.mensaje).toBe('No hay insignias.');
+                }
+            })
     })
 })
