96c2b4ab37c1b552517ae2fb60d1d21845f1b024
jesrolcad <jesrolcad@alum.us.es>
2022-08-20 13:57:30 +0200

    TESTS DE COMPONENTE DE FILTROS
    Se ha testeado el componente Filtros.vue
    
    Roldán Cadena, Jesús <@jesrolcad>

---
+++ tests/unit/programas/filtros.spec.js
@@ -0,0 +1,69 @@
+import { shallowMount } from '@vue/test-utils';
+import Filtros from 'Filtros.vue';
+import flushPromises from 'flush-promises';
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from "@/router"
+
+const mockGenerosInicial = ['Terror', 'Fantasía'];
+
+const mockGenerosDefinitivo = ['Ciencia ficción', 'Thriller', 'Comedia'];
+
+const programasFiltrados = ['programa1', 'programa2', 'programa3'];
+
+describe('TESTS DE FILTROS', () => {
+
+    beforeEach(() => {
+        fetch.resetMocks();
+        jest.restoreAllMocks();
+    })
+
+    describe('TESTS MÉTODO GETGENEROS', () => {
+
+        it('CASO GENERAL', async () => {
+
+            let spyGetGeneros = jest.spyOn(Filtros.methods, 'getGeneros');
+
+            fetch.mockResponses([
+                JSON.stringify(mockGenerosInicial),
+            ], [
+                JSON.stringify(mockGenerosDefinitivo)
+            ])
+
+            let wrapper = shallowMount(Filtros);
+
+            await flushPromises();
+
+            expect(spyGetGeneros).toHaveBeenCalledTimes(1);
+            expect(wrapper.vm.allgeneros.length).toBe(2);
+            wrapper.vm.getGeneros();
+            await flushPromises();
+            expect(wrapper.vm.allgeneros.length).toBe(3);
+
+        });
+
+    describe('TESTS MÉTODO FILTRARPROGRAMAS', () => {
+            
+            it('CASO GENERAL', async () => {
+    
+                fetch.mockResponses([
+                    JSON.stringify(mockGenerosInicial),
+                ], [
+                    JSON.stringify(mockGenerosDefinitivo)
+                ])
+    
+                let wrapper = shallowMount(Filtros);
+    
+                await flushPromises();
+    
+                await wrapper.vm.filtrarProgramas();
+                expect(wrapper.vm.filtrados.length).toBe(3);
+                expect(wrapper.emitted().escucharFiltros).toBeTruthy();
+    
+            });
+    
+    })
+
+
+    })
+
+})
